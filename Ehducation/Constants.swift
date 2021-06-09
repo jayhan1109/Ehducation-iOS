e55852b93a172818e98aaa5dd086113ce5c77402
jayhan1109 <jungho1109@gmail.com>
2021-06-09 14:35:02 -0700

    card background color

---
+++ Ehducation/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Ehducation
+//
+//  Created by Jeongho Han on 2021-06-03.
+//
+
+import Foundation
---
+++ Ehducation/View/MainPage/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  Ehducation
+//
+//  Created by Jeongho Han on 2021-06-03.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
