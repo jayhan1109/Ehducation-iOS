141dc7398088c76b40565cdb051e95e06e514721
jayhan1109 <jungho1109@gmail.com>
2021-06-09 21:34:51 -0700

    Add Image function

--- Ehducation/Controller/NewPostViewController.swift
+++ Ehducation/Controller/NewPostViewController.swift
@@ -13,16 +13,22 @@ class NewPostViewController: UIViewController {
     @IBOutlet weak var postButton: UIButton!
     @IBOutlet weak var gradeButton: UIButton!
     @IBOutlet weak var subjectButton: UIButton!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
     
     let gradeDropdown = DropDown()
     let subjectDropdown = DropDown()
     
-    var itemProviders: [NSItemProvider] = []
-    var iterator: IndexingIterator<[NSItemProvider]>?
+    var imageViews: [UIImageView] = []
+    var clearViews: [UIButton] = []
+    var images: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationController?.navigationBar.barTintColor = UIColor(named: K.Colors.pageBackgroundColor)
         
         postButton.layer.cornerRadius = postButton.frame.height / 5
@@ -42,8 +48,11 @@ class NewPostViewController: UIViewController {
         gradeDropdown.selectionAction = { [unowned self] (index: Int, item: String) in
             gradeButton.setTitle(item, for: .normal)
         }
+        
+        imageViews = [imageView1, imageView2, imageView3, imageView4, imageView5]
+        
     }
-
+    
     @IBAction func gradePressed(_ sender: UIButton) {
         gradeDropdown.show()
     }
@@ -54,37 +63,51 @@ class NewPostViewController: UIViewController {
     @IBAction func addImageButton(_ sender: UIButton) {
         if #available(iOS 14, *) {
             var configuration = PHPickerConfiguration()
-            configuration.selectionLimit = 0
+            configuration.selectionLimit = 5
             configuration.filter = .any(of: [.images, .videos])
             
             let picker = PHPickerViewController(configuration: configuration)
             picker.delegate = self
             self.present(picker, animated: true, completion: nil)
+            
+            print(images.count)
         } else {
             // Fallback on earlier versions
         }
     }
+    
+
+    
 }
 
 extension NewPostViewController: PHPickerViewControllerDelegate {
     
-    
     @available(iOS 14, *)
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         
-        picker.dismiss(animated: true) // 1
+        picker.dismiss(animated: true)
         
-        let itemProvider = results.first?.itemProvider // 2
+        for imgView in imageViews{
+            imgView.image = .none
+        }
         
-        if let itemProvider = itemProvider,
-           itemProvider.canLoadObject(ofClass: UIImage.self) { // 3
-            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in // 4
-                DispatchQueue.main.async {
-                    print(image)
+        images = []
+        
+        for idx in 0..<results.count{
+            let itemProvider = results[idx].itemProvider
+            
+            if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                    if let img = image as? UIImage{
+                        DispatchQueue.main.async {
+                            self.imageViews[idx].image = img
+                            self.images.append(img)
+                        }
+                    }
                 }
+            } else {
+                // TODO: Handle empty results or item provider not being able load UIImage
             }
-        } else {
-            // TODO: Handle empty results or item provider not being able load UIImage
         }
     }
 }
